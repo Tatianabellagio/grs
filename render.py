0d5eb3c8b38f76913850585eaabc2fd7e5a43fa9
TBellagio <tbellagio@corebi.com.ar>
2020-06-09 16:11:49 -0300

    coneccion a base de datos

--- render.py
+++ render.py
@@ -4,33 +4,18 @@ import base64
 import dash_core_components as dcc
 import dash_html_components as html
 
-from dash.exceptions import PreventUpdate
-
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-from flask import Flask, render_template
-
-from flask_mysqldb import MySQL 
 
 external_stylesheets = [dbc.themes.BOOTSTRAP,] #'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
 
-app.server.config["MYSQL_HOST"] = "127.0.0.1"
-app.server.config["MYSQL_USER"] = "tatianabellagio"
-app.server.config["MYSQL_PASSWORD"] = "Aa123456!"
-app.server.config["MYSQL_DB"] = "grs_datos"
-mysql = MySQL(app.server)
-#mysql.init_app(app.server)
 
 image_filename = 'Logo_White.png' # replace with your own image
-
-
-
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 trabajadores = ["Médico/a",
@@ -42,8 +27,6 @@ trabajadores = ["Médico/a",
 "Seguridad",
 "Personal que maneja la ropa",
 "Personal de Cocina"]
-
-centros_salud = pd.read_csv("nombre_centros.csv",header=None, index_col = 0)[1]
 
 colors = {
     'background': '#9fd5d1',
@@ -102,7 +85,7 @@ children = [
                         ),width=6  ,  ),
 
                 dbc.Col(dcc.Dropdown(id = "institucion_trabajo", 
-                                    options=[{'label':i, 'value': j} for i,j in zip (centros_salud,centros_salud)],
+                                    options=[{'label':i, 'value': j} for i,j in zip (["lugar1","lugar2"],["lugar1","lugar2"])],
                                     value=''
                         ),width=6 , style = style_letter  ),
             ]
@@ -121,7 +104,7 @@ children = [
                         ),width=6 , style = style_letter  ),
             ]
         ),
-                dbc.Row(
+        dbc.Row(
             [
                 dbc.Col( html.H1("¿Tenés contacto con pacientes antes de que se determine su diagnóstico?", style= style_letter
                         ),width= 6 ,  ),
@@ -295,7 +278,6 @@ children = [
             ]
         ),
 
-
         html.Div(dbc.Button(id='submit-button-state', n_clicks=0, children='Enviar información',
                              outline=True, color="secondary",size="lg"),
          style={'margin-left': 450, "margin-top": 25 }),
@@ -326,22 +308,37 @@ children = [
     State('parte_iniciativa', 'value'),
     State("contacto", "value")])
 
-def cargo_tabla(n_clicks,rol_establecimiento,institucion_trabajo, contacto_sospechosos,contacto_prediagnostico,equipamiento_nec,barbijos,guantes,batas,gafas,barbijos_n95,farmacos,input_farmacos,input_insumo_prioritario,
+def cargo_tabla(n_clicks,rol_establecimiento,institucion_trabajo,
+contacto_sospechosos,contacto_prediagnostico,equipamiento_nec,barbijos,guantes,batas,gafas,barbijos_n95,farmacos,input_farmacos,input_insumo_prioritario,
 grupo_riesgo,parte_iniciativa, contacto):
-    if n_clicks == 0:
-        raise PreventUpdate
-        rta = ""
-    else:
-        cur = mysql.connection.cursor()
-        cur.execute("""insert into trabajadores_centros_salud (rol_establecimiento, institucion_trabajo,contacto_sospechosos,
-        contacto_prediagnostico,equipamiento_nec,barbijos,guantes,batas,gafas,barbijos_n95,farmacos,input_farmacos,
-        input_insumo_prioritario,grupo_riesgo,parte_iniciativa, contacto) VALUES (%s, %s, %s,%s, %s, %s, %s, %s, %s,
-        %s, %s, %s,%s, %s, %s, %s)""",(rol_establecimiento,
-         institucion_trabajo, contacto_sospechosos,contacto_prediagnostico,equipamiento_nec,barbijos,guantes,batas,gafas,
-         barbijos_n95,farmacos,input_farmacos,input_insumo_prioritario,grupo_riesgo,parte_iniciativa, contacto))
-        mysql.connection.commit()
-        rta = "Gracias por responder!"
-    return rta
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    df = pd.read_csv("datos_covidlab.csv", usecols = ['rol_establecimiento', 'institucion_trabajo',
+       'contacto_sospechosos', 'contacto_prediagnostico', 'equipamiento_nec',
+       'barbijos', 'guantes', 'batas', 'gafas', 'barbijos_n95', 'farmacos',
+       'input_farmacos', 'input_insumo_prioritario', 'grupo_riesgo',
+       'parte_iniciativa', "contacto"]).dropna(how="all")
+    
+
+    diccionario = {'rol_establecimiento': rol_establecimiento,
+    'institucion_trabajo':institucion_trabajo,
+    'contacto_sospechosos':contacto_sospechosos,
+    'contacto_prediagnostico':contacto_prediagnostico,
+    'equipamiento_nec':equipamiento_nec,
+    'barbijos':barbijos,
+    'guantes':guantes,
+    'batas':batas,
+    'gafas':gafas,
+    'barbijos_n95':barbijos_n95,
+    'farmacos':farmacos,
+    'input_farmacos':input_farmacos,
+    'input_insumo_prioritario':input_insumo_prioritario,
+    'grupo_riesgo':grupo_riesgo,
+    'parte_iniciativa':parte_iniciativa,
+    "contacto" : contacto}
+
+    df = df.append(diccionario, ignore_index=True)
+    df.to_csv("datos_covidlab.csv")
+
+
+    rta = "Gracias por responder!"
+    return rta
